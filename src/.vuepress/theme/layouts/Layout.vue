2defec53eb3f875fe0a40816bea3c4ab8a2ef56d
Christopher Krawietz <krawietz@new-communication.de>
2021-02-25 15:41:06 +0100

    added own theme layout file and updated gitignore

---
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <ParentLayout>
+    <template #sidebar-top>
+
+    </template>
+    <template #page-bottom>
+
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    ParentLayout,
+  }
+}
+</script>
