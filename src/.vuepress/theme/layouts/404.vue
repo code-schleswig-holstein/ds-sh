45af9d870ad003a36600eea8d0e70cdda2069bc8
Christopher Krawietz <krawietz@new-communication.de>
2021-04-06 11:54:50 +0200

    added 404 content

--- src/.vuepress/theme/layouts/404.vue
+++ src/.vuepress/theme/layouts/404.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="theme-container">
     <div class="theme-content">
-      <h1>404</h1>
-
-      <blockquote>{{ getMsg() }}</blockquote>
-
-      <RouterLink to="/">
-        Take me home.
-      </RouterLink>
+      <div class="u-background--gradient-itvsh-10 u-paddingTop--r4 u-paddingBottom--r4">
+        <div class="u-container">
+          <section class="u-limitWidth-12 u-align-center">
+            <div class="GridCol1Main-row u-row">
+              <div class="GridCol1Main-col u-col u-stack--2">
+                <lottie-animation class="u-align-center"  :path="$withBase('./img/development.json')" :speed="0.8" :width="256"/>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+      <div class="u-background--white u-paddingTop--r4 u-paddingBottom--r4">
+        <div class="u-container">
+          <section class="u-limitWidth-6 u-align-center">
+            <div class="GridCol1Main-row u-row">
+              <div class="GridCol1Main-col u-col u-stack--2">
+                <h1 id="main-title" class="Base-h1">Nanu?<br/>Der gewünschte Inhalt wurde nicht an der erwarteten Stelle gefunden</h1>
+                <p class="Base-p description">Dafür kann es viele Gründe geben: Seiten wurden verschoben, umbenannt, gelöscht oder jemand hat auf dem Server mal zu gründlich feucht durchgewischt.<br/><br/> Nutzen Sie einfach die Suche, um den gewünschten Inhalt an seinem neuen Ort zu finden.</p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
-<script>
-const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
-
-export default {
-  methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
-  }
-}
-</script>
